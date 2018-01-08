20157b073d4a694342937451e5cf67d2972d1ec3
LarsTijsmans <l.j.tijsmans@students.uu.nl>
2018-01-08 10:05:07 +0100

    external storage

--- src/android/FileManager.java
+++ src/android/FileManager.java
@@ -80,8 +80,8 @@ public class FileManager {
      * @return the file directory
      */
     private File getFileDirectory() {
-        //File tempFile = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-        File tempFile = new File(mContext.getFilesDir().getAbsolutePath());
+        File tempFile = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        //File tempFile = new File(mContext.getFilesDir().getAbsolutePath());
 
         File audioPath = new File(tempFile, FOLDER_NAME_SAVE_AUDIO);
 
